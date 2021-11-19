fa6e46e32add94437c95323da7f5bcf5a5af3951
thomas-rooty <essainono1@gmail.com>
2021-11-19 11:28:01 +0100

    created file submitter project

---
+++ document-submitter/src/webparts/documentSubmitter/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IDocumentSubmitterWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'DocumentSubmitterWebPartStrings' {
+  const strings: IDocumentSubmitterWebPartStrings;
+  export = strings;
+}
--- react-file-upload/lib/webparts/fileUpload/components/FileUpload.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-import { IFileUploadProps } from './IFileUploadProps';
-export default class FileUpload extends React.Component<IFileUploadProps, {}> {
-    constructor(props: IFileUploadProps);
-    render(): React.ReactElement<IFileUploadProps>;
-}
