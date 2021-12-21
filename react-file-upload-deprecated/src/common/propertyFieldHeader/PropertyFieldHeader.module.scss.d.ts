aa53ab98cfc31635f44b93dd373f8712d6e60b9a
CARON Thomas <essainono1@gmail.com>
2021-12-21 09:07:15 +0100

    Delete react-file-upload-deprecated directory

--- react-file-upload-deprecated/src/common/propertyFieldHeader/PropertyFieldHeader.module.scss.d.ts
+++
@@ -1,7 +0,0 @@
-declare const styles: {
-    headerBar: string;
-    header: string;
-    info: string;
-    headerCallout: string;
-};
-export default styles;
--- react-file-upload-deprecated/src/webparts/fileUpload/components/FileUpload.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { IFileUploadProps } from './IFileUploadProps';
-export default class FileUpload extends React.Component<IFileUploadProps, {}> {
-    constructor(props: IFileUploadProps);
-    render(): React.ReactElement<IFileUploadProps>;
-}
