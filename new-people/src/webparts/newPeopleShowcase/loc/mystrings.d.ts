907810ca0e378beb4b13892eee97052af901c44f
thomas-rooty <essainono1@gmail.com>
2022-01-10 15:58:29 +0100

    created NewPeopleShowcase

---
+++ new-people/src/webparts/newPeopleShowcase/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface INewPeopleShowcaseWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'NewPeopleShowcaseWebPartStrings' {
+  const strings: INewPeopleShowcaseWebPartStrings;
+  export = strings;
+}
